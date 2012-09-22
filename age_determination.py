dd73774a6f0466b63300ef0cbe74fb38a7e46d32
eidelen <adrian.schneider@eidelen.ch>
2012-09-22 22:06:15 +0200

    Remove unused code

--- age_determination.py
+++ age_determination.py
@@ -9,41 +9,32 @@ import numpy as np
 from skimage import measure
 from skimage import filter
 from skimage import data
-from skimage.filter import threshold_otsu
+from skimage.filter import *
 #from skimage.segmentation import find
 from skimage.morphology import label, closing, square
 from skimage.measure import regionprops
      
 def extract_Bones( pilImage ):
-    
-    
-    #edges = filter.sobel( pilImage )
-    
-    # Find contours at a constant value of 0.8
-    contours = measure.find_contours( pilImage, 5000)
-    
-    # Display the image and plot all contours found
-    #plt.imshow( pilImage, interpolation='nearest')
-    
-    for n, contour in enumerate(contours):
-        plt.plot(contour[:, 1], contour[:, 0], linewidth=2)
-    
-    plt.title('findcountours', fontsize=20)
-    plt.xticks([])
-    plt.yticks([])
-    plt.show()
 
-
-    #thresh = threshold_otsu( pilImage )
-    thresh = get_Threshold( pilImage )
+    thresh = get_XRay_BG_Threshold( pilImage )
     treshMask = pilImage > thresh
     plt.imshow( treshMask )
-    plt.title('own thresh', fontsize=20)
+    plt.title('threshold mask', fontsize=20)
     plt.show()
     
+    #get_Fingers(treshMask)
+    
     maskedImage = pilImage * treshMask
+    
+    adaptiveMask = threshold_adaptive(maskedImage, 15, 'mean')
+    
+    plt.imshow( adaptiveMask )
+    plt.title('adaptive mask', fontsize=20)
+    plt.show()
+    
+    maskedImage = pilImage * adaptiveMask
     plt.imshow( maskedImage )
-    plt.title('masked Image', fontsize=20)
+    plt.title('adaptive masked image', fontsize=20)
     plt.show()
     
     cannyS5 = filter.canny(maskedImage, 3)
@@ -51,46 +42,15 @@ def extract_Bones( pilImage ):
     plt.title('tresh - canny sigma 3', fontsize=20)
     plt.show()
     
-    
-    
-    
-    image = pilImage
-
-    # apply threshold
-    thresh = threshold_otsu(image)
-    bw = closing(image > thresh, square(3))
-    
-    # remove artifacts connected to image border
-    cleared = bw.copy()
-    #clear_border(cleared)
-    
-    # label image regions
-    label_image = label(cleared)
-    borders = np.logical_xor(bw, cleared)
-    label_image[borders] = -1
-    
-    fig, ax = plt.subplots(ncols=1, nrows=1, figsize=(6, 6))
-    ax.imshow(label_image, cmap='jet')
-    
-    for region in regionprops(label_image, ['Area', 'BoundingBox']):
-    
-        # skip small images
-        if region['Area'] < 100:
-            continue
-    
-        # draw rectangle around segmented coins
-        minr, minc, maxr, maxc = region['BoundingBox']
-        rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
-                                  fill=False, edgecolor='red', linewidth=2)
-        ax.add_patch(rect)
-    
-    plt.title('regions', fontsize=20)
-    plt.show()
-    
     return pilImage
     
     
-def get_Threshold( pilImage ):
+#def get_Fingers( binaryImage ):
+   
+#def remove_first_uniform_layer( mask, grayImage ):
+    # region growing seeded at the masks boarder
+      
+def get_XRay_BG_Threshold( pilImage ):
     
     grayhist, bins = np.histogram(pilImage.flatten(),  200 )
    
@@ -106,7 +66,8 @@ def get_Threshold( pilImage ):
     val1 = bins[idx0]
     val2 = bins[idx1]
 
-    threshVal = (val1 + val2) / 2.0
+    # set threshold closer to lower peak
+    threshVal = (2*val1 + val2) / 3.0
     
     #plt.plot(grayhist)
     #plt.show()
