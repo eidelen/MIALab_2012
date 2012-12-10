98943a1cab0f7caa9bcc5ba090f77b6604658a61
bbmedia <jonas@realboarder.ch>
2012-12-10 17:17:51 +0100

    - crop template matching sample.

--- classify/templateMatchingClassifier.py
+++ classify/templateMatchingClassifier.py
@@ -1,5 +1,5 @@
 from classify.base import jointClassifier
-
+import Image
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage import data
@@ -14,8 +14,13 @@ class templateMatchingClassifier(jointClassifier):
     def classify(self,sample):
         
         scores = []
-        for classImage in self.classImages:
-            score = match_template(classImage, sample,1)
+        for classImage in self.classImages:      
+            
+            #plt.imshow(sample[10:70,20:60],cmap=plt.cm.gray)
+            #plt.show()
+            
+            #do template matching
+            score = match_template(classImage, sample[10:70,20:60],1)
             scores.append(np.max(score))
         
         classification = self.classLabels[np.argmax(scores)]
